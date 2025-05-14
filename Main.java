d9124d654a885eac45b3aea8bcc1ef5f61c02232
Guilherme Carvalho de Oliveira <163038461+guiCarvalhoOliveira@users.noreply.github.com>
2025-05-14 18:21:40 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        // Criando duas contas
+        
         ContaBancaria conta1 = new ContaBancaria(101, "Pedro", 500.0);
         ContaBancaria conta2 = new ContaBancaria(102, "Guilherme", 1000.0);
 
-        // Operações na conta1
+        
         System.out.println("Saldo inicial da conta1: " + conta1.getSaldo());
         conta1.depositar(200.0);
         System.out.println("Saldo após depósito na conta1: " + conta1.getSaldo());
@@ -14,7 +14,7 @@ public class Main {
 
         System.out.println("-------------------------");
 
-        // Operações na conta2
+        
         System.out.println("Saldo inicial da conta2: " + conta2.getSaldo());
         conta2.depositar(500.0);
         System.out.println("Saldo após depósito na conta2: " + conta2.getSaldo());
